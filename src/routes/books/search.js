148af8ea14415c6c46aa2a962c5296661d4b516a
sumit-ch-bjit <sumit.chakraborty@bjitacademy.com>
2023-09-14 08:07:02 +0600

    added initial files

---
+++ src/routes/books/search.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllBooks,
+  addBook,
+  getBookById,
+} = require("../../controllers/bookController");
+const {
+  bookValidationRules,
+  validate,
+} = require("../../middleware/validation");
+
+// Define book routes here
+
+// GET /books
+router.get("/", getAllBooks);
+router.get("/:id", getBookById);
+router.post("/", bookValidationRules(), validate, addBook);
+
+module.exports = router;
---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const search = require("./books/search");
+
+const constructorMethod = (app) => {
+  app.use("/search", search);
+
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Not found" });
+  });
+};
+
+module.exports = constructorMethod;
